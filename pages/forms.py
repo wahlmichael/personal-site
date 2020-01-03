dc7b3005f3d05f8f3e935ec6a8cd27bce3942d43
Michael Wahl <michaelwahl9@gmail.com>
2020-01-02 21:08:54 -0600

    fix styling for contact form

--- pages/forms.py
+++ pages/forms.py
@@ -1,10 +1,31 @@
 from django import forms
 
 class EmailForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-    your_email = forms.EmailField(max_length=254)
+    your_name = forms.CharField(
+        max_length=100,
+        label='',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control px-4 py-3 my-3',
+                'placeholder': 'Your Name'
+            }
+        ))
+    your_email = forms.EmailField(
+        max_length=254,
+        label='',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control px-4 py-3 my-3',
+                'placeholder': 'Your Email'
+            }
+        ))
     your_message = forms.CharField(
         max_length=2000,
-        widget=forms.Textarea(),
-        help_text='Write here your message!'
-    )
+        label='',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control px-4 py-3 my-3',
+                'placeholder': 'Write a Message',
+                }
+            )
+        )
--- pages/views.py
+++ pages/views.py
@@ -8,21 +8,18 @@ def index(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['your_name'])
             message_to_send = 'Message from {} at email {}: {}'.format(
                 form.cleaned_data['your_name'],
                 form.cleaned_data['your_email'],
                 form.cleaned_data['your_message']
             )
             send_mail('message from website',
-                    message_to_send,
-                    'michaelwahldev@gmail.com',
-                    ['michaelwahl9@gmail.com'])
-            print(message_to_send)
+                      message_to_send,
+                      'michaelwahldev@gmail.com',
+                      ['michaelwahl9@gmail.com'])
             return HttpResponseRedirect("/")
     else:
         form = EmailForm()
-
     projects = Portfolio.objects.all()
     context = {
         'form': form,
